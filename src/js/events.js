398be8521430e37d989ea06440e0bf9442098f08
einartech <imalovelyghost13@gmail.com>
2024-12-16 10:28:05 +0100

    getting ready workspace to init with modal functionallity, es6 modals added, html formatted and first functionality of test done

--- src/js/events.js
+++ src/js/events.js
@@ -1,24 +1,80 @@
-// formulario de inicio
-document.getElementById('start-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+import { printVar,typeVar } from '/src/js/helper.js';
+import { validatePlayerName } from '/src/js/functions.js';
+// import {add, reset, decrease} from './counter.js'
 
-    // Obtener nombre del jugador
-    const playerName = document.getElementById('player-name').value.trim();
 
-    // Validar el nombre 
-    if (validatePlayerName(playerName)) {
-        // Si es válido, actualizamos el nombre del jugador y comenzamos el juego
-        updatePlayerName(playerName);
-        startGame();
-    } else {
-        // Si es inválido, alerta
-        alert("Por favor, ingresa solo letras en el nombre.");
-    }
-});
+//PASOS A SEGUIR!
+//1.- AL CLICAR EL BOTON, DEBE HACER UN CONSOLE.LOG OK
+//1.- DIVIDIR ZONAS, LOS EVENT LISTENERS EN EVENTS Y LAS FUNCIONES EN EL OTRO LADO
 
-// Botón de reinicio del juego
-document.getElementById('reset-btn').addEventListener('click', function() {
-    resetGame();
-});
+// let formPlayerName=document.getElementById("player-name");
+// console.log(formPlayerName)
+
+// let modalPlayerInfo=document.getElementById("openmodal");
+// modalPlayerInfo.addEventListener("click", validatePlayerName);
+
+let formPlayerName = document.getElementById("start-form");
+formPlayerName.addEventListener("submit", validatePlayerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // formulario de inicio
+// document.getElementById('start-form').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     // Obtener nombre del jugador
+//     const playerName = document.getElementById('player-name').value.trim();
+
+//     // Validar el nombre 
+//     if (validatePlayerName(playerName)) {
+//         // Si es válido, actualizamos el nombre del jugador y comenzamos el juego
+//         updatePlayerName(playerName);
+//         startGame();
+//     } else {
+//         // Si es inválido, alerta
+//         alert("Por favor, ingresa solo letras en el nombre.");
+//     }
+// });
+
+// // Botón de reinicio del juego
+// document.getElementById('reset-btn').addEventListener('click', function() {
+//     resetGame();
+// });
+
+
 
 
---
+++ src/js/functions.js
@@ -0,0 +1,55 @@
+function validatePlayerName(event) {
+    event.preventDefault();  // Evita que el formulario se envíe
+
+    console.log("Has insertado un playername");
+}
+
+
+
+
+
+
+
+
+
+export {validatePlayerName}
+
+
+
+
+
+
+
+
+
+
+// //Validar el nombre del jugador 
+// function validatePlayerName(playerName) {
+//     return /^[A-Za-z]+$/.test(playerName); // Validar solo letras
+// }
+
+// // Cambiar el nombre del jugador 
+// function updatePlayerName(playerName) {
+//     console.log(playerName)
+
+//     document.querySelector('.player-name').textContent = playerName;
+// }
+
+
+// // Iniciar el juego 
+// function startGame() {
+//     document.querySelector('.start-game-container').style.display = 'none';
+//     document.getElementById('game-container').style.display = 'block';
+// }
+
+// // Reiniciar el juego 
+// function resetGame() {
+//     // Mostrar el formulario de inicio y ocultar el juego
+//     document.querySelector('.start-game-container').style.display = 'block';
+//     document.getElementById('game-container').style.display = 'none';
+    
+//     // Limpiar el campo de nombre
+//     document.getElementById('player-name').value = '';
+//     document.querySelector('.player-name').textContent = ''
+// }
+
